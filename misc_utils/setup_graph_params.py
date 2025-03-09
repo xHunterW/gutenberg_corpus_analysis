9e6d56caa0a1e0224c15391a9669a3f91a5c9f85
Dean Weiss <weiss.dean@gmail.com>
2025-03-09 17:35:05 -0400

    Add docstrings to setup_graph_params.py

--- misc_utils/setup_graph_params.py
+++ misc_utils/setup_graph_params.py
@@ -1,6 +1,12 @@
+"""Set up default graphing parameters
+
+This module contains functions that are used by multiple graphs in the our SPGC analysis
+"""
 import numpy as np
 
 def get_graph_params():
+    """Returns default parameters for graph generation
+    """
     ###########
     ## Setup ##
     ###########
@@ -11,7 +17,7 @@ def get_graph_params():
     fig_width = fig_width_pt*inches_per_pt  # width in inches
     fig_height = width_vs_height*fig_width  # height in inches
     fig_size = [fig_width,fig_height]
-    
+
     # here you can set the parameters of the plot (fontsizes,...) in pt
     params = {'backend': 'ps',
               'axes.titlesize':16,
@@ -21,16 +27,16 @@ def get_graph_params():
     #           'figtext.fontsize': 12,
               'xtick.labelsize': 12,
               'ytick.labelsize': 12,
-              
+
      #         'text.usetex': True,
      #         'ps.usedistiller' : 'xpdf',
               'figure.figsize': fig_size,
     #          'text.latex.unicode':True,
     #          'text.latex.preamble': [r'\usepackage{bm}'],
-              
+
               'xtick.direction':'out',
               'ytick.direction':'out',
-              
+
               'axes.spines.right' : False,
               'axes.spines.top' : False
              }
